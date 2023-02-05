48e9fdacba94ae9e0618c8487b6edb1fdb8a16c5
mrkevr <markevercantillano@gmail.com>
2023-02-05 22:45:41 +0800

    first commit

---
+++ mrkevr-webpage/mrkevr-webpage/src/main/java/com/mrkevr/mrkevrwebpage/MrkevrWebpageApplication.java
@@ -0,0 +1,15 @@
+package com.mrkevr.mrkevrwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MrkevrWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrkevrWebpageApplication.class, args);
+	}
+	
+}
