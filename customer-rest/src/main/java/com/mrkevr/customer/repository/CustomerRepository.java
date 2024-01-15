2778f6bc16df05ae85bdcba770d86ffb29032e73
mrkevr <markevercantillano@gmail.com>
2024-01-15 15:42:12 +0800

    new page

--- customer-rest/src/main/java/com/mrkevr/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mrkevr.customer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mrkevr.customer.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
-}
--- customer-rest/src/main/java/com/mrkevr/customer/repository/ParcelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mrkevr.customer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mrkevr.customer.entity.Parcel;
-
-public interface ParcelRepository extends JpaRepository<Parcel, Long> {
-
-}
